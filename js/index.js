2117b8feb4487caa01506ae1e9f951c211349aff
Evelyn <ks0430@163.com>
2018-12-29 22:19:17 +1000

    Add js effects. Finish contact page.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,56 @@
 $(document).ready(function() {
   console.log("Document ready!");
+
+  $(window).scroll(() => {
+    // Header: change stick header background
+    // if use arrow function, then this will bind to window object.
+    stickHeaderHandler();
+  });
+
+  // Input focus label change handler
+  inputFocusHandler();
+  navbarLinkHandler();
 });
+
+// header stick handler - bind to document object
+stickHeaderHandler = () => {
+  if (window.pageYOffset >= 30) {
+    $("#header").addClass("b-header--white");
+  } else {
+    $("#header").removeClass("b-header--white");
+  }
+};
+
+inputFocusHandler = () => {
+  let input = $(".b-form__input, .b-form__textarea"); // input & textarea array
+
+  input.focusin(function() {
+    $(this)
+      .siblings(".b-form__label")
+      .addClass("b-form__label--focus");
+    $(this)
+      .siblings(".b-form__icon")
+      .addClass("");
+  });
+
+  input.focusout(function() {
+    $(this)
+      .siblings(".b-form__label")
+      .removeClass("b-form__label--focus");
+
+    if ($(this).val() !== "") {
+      $(this)
+        .siblings(".b-form__label")
+        .addClass("b-form__label--focus");
+    }
+  });
+};
+
+navbarLinkHandler = () => {
+  let link = $(".b-navbar__link");
+  console.log(link);
+  link.click(function() {
+    link.removeClass("b-navbar__link--active");
+    $(this).addClass("b-navbar__link--active");
+  });
+};
