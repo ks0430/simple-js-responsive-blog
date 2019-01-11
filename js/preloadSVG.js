78f35a416daa0597e8f0930a59288f27e8f5499a
Evelyn <ks0430@163.com>
2019-01-11 12:49:35 +1000

    Add ajax preload script.

--- js/preloadSVG.js
+++ js/preloadSVG.js
@@ -1,50 +1,15 @@
 "use strict";
 
 const preloadSVG = () => {
-  var file = "/img/portfolio/icons/sprite.svg",
-    revision = 1;
-
-  if (
-    !document.createElementNS ||
-    !document.createElementNS("http://www.w3.org/2000/svg", "svg").createSVGRect
-  )
-    return true;
-
-  var isLocalStorage =
-      "localStorage" in window && window["localStorage"] !== null,
-    request,
-    data,
-    insertIT = function() {
-      document.body.insertAdjacentHTML("afterbegin", data);
-    },
-    insert = function() {
-      if (document.body) insertIT();
-      else document.addEventListener("DOMContentLoaded", insertIT);
-    };
-
-  if (isLocalStorage && localStorage.getItem("inlineSVGrev") == revision) {
-    data = localStorage.getItem("inlineSVGdata");
-    if (data) {
-      insert();
-      return true;
-    }
-  }
-
-  try {
-    request = new XMLHttpRequest();
-    request.open("GET", file, true);
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400) {
-        data = request.responseText;
-        insert();
-        if (isLocalStorage) {
-          localStorage.setItem("inlineSVGdata", data);
-          localStorage.setItem("inlineSVGrev", revision);
-        }
-      }
-    };
-    request.send();
-  } catch (e) {}
+  const ajax = new XMLHttpRequest();
+  ajax.open("GET", "img/portfolio/icons/sprite.svg");
+  ajax.send();
+  ajax.onload = function(event) {
+    let div = document.createElement("div");
+    div.innerHTML = ajax.responseText;
+    document.body.insertBefore(div, document.body.childNodes[0]);
+    console.log(div);
+  };
 };
 
 export default preloadSVG;
